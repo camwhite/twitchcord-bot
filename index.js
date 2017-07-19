83404d7a358bdae40cc0cfddc6f71a4d701b34c5
Cam White <camw85@mac.com>
2017-07-18 23:07:49 -0600

    :heavy_check_mark: basic message relay

---
+++ index.js
@@ -0,0 +1,4 @@
+'use strict'
+
+const Bot = require('./utils/bot')
+const bot = new Bot()
---
+++ utils/bot.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const { twitch, discord } = require('./config')
+
+const chalk = require('chalk')
+const tmi = require('tmi.js')
+const Discord = require('discord.js')
+
+const twitchClient = new tmi.client(twitch)
+const discordClient = new Discord.Client()
+
+class Bot {
+  constructor () {
+    twitchClient.connect()
+    discordClient.login(discord.token)
+
+    // Twitch client listeners
+    twitchClient.on('message', (channel, userstate, message, self) => {
+      //discordClient.user.setUsername('twitchcord-bot')
+
+      console.log(`${chalk.green(userstate['display-name'])}: ${message}`)
+
+      if (userstate['display-name'] !== twitch.identity.username) {
+        this.sendToDiscord({ userstate, message })
+      }
+    })
+    twitchClient.on('join', (channel, username, self) => {
+      const userstate = { 'display-name': username }
+      const message = 'has joined the stream :O'
+
+      if (this.channel) {
+        this.sendToDiscord({ userstate, message })
+      }
+    })
+
+    // Discord client listeners
+    discordClient.on('ready', () => {
+      this.channel = discordClient.channels.find('name', discord.channel)
+    })
+    discordClient.on('message', (message) => {
+      if (message.author.username === twitch.identity.username) {
+        this.sendToTwitch(message)
+      }
+    })
+  }
+  /**
+  * Sends a message from twitch irc to discord.
+  * @param {object} the message data
+  * @return {promise} the sent message
+  */
+  async sendToDiscord ({ userstate, message }) {
+    let sent
+    try {
+      sent = await this.channel.send(`**${userstate['display-name']}** ${message}`)
+    } catch (err) {
+      throw new Error(err)
+    }
+
+    return sent
+  }
+  /**
+  * Sends a message to twitch irc from discord.
+  * @param {object} the message data
+  * @return {promise} the sent message
+  */
+  async sendToTwitch ({ content, author }) {
+    let sent
+    try {
+      sent = await twitchClient.say(author.username, content)
+    } catch (err) {
+      throw new Error(err)
+    }
+
+    return sent
+  }
+}
+
+module.exports = Bot
